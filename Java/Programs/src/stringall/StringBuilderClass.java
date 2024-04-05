972e1e0f0bf300f0d8b7318c238a708da42ff2ac
Kalyan <pkalyanjs@gmail.com>
2024-04-05 15:49:51 +0530

    String code has been addeed

--- Java/Programs/src/stringall/StringBuilderClass.java
+++ Java/Programs/src/stringall/StringBuilderClass.java
@@ -16,11 +16,25 @@ public class StringBuilderClass {
 	public static void printInformation(StringBuilder builder) {
 		System.out.println("StringBuilder = " + builder);
 		System.out.println("length = " + builder.length());
-	}
-
-	public static void main(String[] args) {
+		
+		StringBuilder builderPlus = new StringBuilder("Hello" + "World");
+		builderPlus.append("and GoodBye");
+		
+		builderPlus.deleteCharAt(16).insert(16, 'g');
+		System.out.println(builderPlus);
+		
+		builderPlus.replace(16, 17, "G");
+		
+		
+		builderPlus.reverse().setLength(7);
+		System.err.println(builderPlus);
+		
+		
+		
 		
 		String helloWorld = "Hello"  + "World";
+		
+		helloWorld.concat("and GoodBye");
 		
 		// We cant assign String literal to StringBuilder Variable
 		// For this We have to assign a constructor
@@ -28,6 +42,7 @@ public class StringBuilderClass {
 		
 		
 		StringBuilder helloWorldBuilder = new StringBuilder("Hello" + "World");
+		helloWorldBuilder.append(" and GoodBye");
 		printInformation(helloWorld);
 		printInformation(helloWorldBuilder);
 
