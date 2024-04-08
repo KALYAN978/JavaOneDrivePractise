10719814878d89f7a1cd2ff2062767637a4c31ec
Kalyan <pkalyanjs@gmail.com>
2024-04-08 12:50:39 +0530

    inheritance practise code has been added

--- Java/Oops/src/inheritance/Dog.java
+++ Java/Oops/src/inheritance/Dog.java
@@ -3,14 +3,14 @@ package inheritance;
 public class Dog extends Animal{
 	
 	
-	public Dog() {
-		super();
-	}
-	
-	
-	public Dog() {
-		super("Mutt","Big",50);
-	}
+//	public Dog() {
+//		super();
+//	}
+//	
+//	
+//	public Dog() {
+//		super("Mutt","Big",50);
+//	}
 
 //	public Dog(String type, double weight, String earShape, String tailShape) {
 //		super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"),weight);
---
+++ Java/Oops/src/inheritance/computerfactory/Main.java
@@ -0,0 +1,5 @@
+package inheritance.computerfactory;
+
+public class Main {
+
+}
