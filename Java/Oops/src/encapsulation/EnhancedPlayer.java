c00a40943911a85cb49a1ba131988d4ec443e294
Kalyan <pkalyanjs@gmail.com>
2024-04-10 14:49:47 +0530

    ENcapsulation code has been added

--- Java/Oops/src/encapsulation/EnhancedPlayer.java
+++ Java/Oops/src/encapsulation/EnhancedPlayer.java
@@ -1,5 +1,27 @@
 package encapsulation;
 
 public class EnhancedPlayer {
+	
+	private String fullName;
+	private int healthPercentage;
+	private String weapon;
+	
+	
+	public EnhancedPlayer(String name) {
+		this(name, 100, "Sword"); 
+	}
+	
+	public EnhancedPlayer(String name,int health, String weapon) {
+		this.fullName = name;
+		if(health <= 0) {
+			this.healthPercentage = 1;
+		}else if(health > 100){
+			this.healthPercentage = 100;
+		}else {
+			this.healthPercentage = health;   
+		}
+		this.weapon  = weapon;
+	}
+	
 
 }
