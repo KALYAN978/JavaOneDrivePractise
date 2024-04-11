cf15a129d07c6cc68595acb7c6be51117469d27f
Kalyan <pkalyanjs@gmail.com>
2024-04-11 12:10:30 +0530

    Encapsualtion code has been added

---
+++ Java/Oops/src/encapsulation/Encapsulation1.java
@@ -0,0 +1,48 @@
+package encapsulation;
+
+public class Encapsulation1 {
+	
+	private long acc;
+	private String name;
+	private String email;
+	private float amount;
+	
+	public long getAcc() {
+		return acc;
+	}
+	
+	
+	public void setAcc(long acc) {
+		this.acc = acc;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public float getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	
+	
+
+}
