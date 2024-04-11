9d359548ca22b94283a5e5eaec3254efb2546934
Kalyan <pkalyanjs@gmail.com>
2024-04-11 11:15:37 +0530

    Encapsulation code has been added

---
+++ Java/Oops/src/encapsulation/Encapsulation0.java
@@ -0,0 +1,28 @@
+package encapsulation;
+
+public class Encapsulation0 {
+	
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		Encapsulation0 o1 = new Encapsulation0();
+		
+		o1.setName("Capgemini");
+
+	}
+
+}
---
+++ Java/Oops/src/encapsulation/EncapsulationReadOnlyClass.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class EncapsulationReadOnlyClass {
+	
+	
+	private String college = "AKG";
+	
+	public String getCollege() {
+		return college;
+	}
+
+}
