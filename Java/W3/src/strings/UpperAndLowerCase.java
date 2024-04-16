916f5c620e1f0af2656644918bb96abf54690f55
Kalyan <pkalyanjs@gmail.com>
2024-04-16 15:12:09 +0530

    Strings practise code has been added

--- Java/W3/src/strings/UpperAndLowerCase.java
+++ Java/W3/src/strings/UpperAndLowerCase.java
@@ -2,13 +2,25 @@ package strings;
 
 public class UpperAndLowerCase {
 	
-	private static String txt = "kjUHdjsuhsHGJH";
+	private static String txt = "kjUHdjsuhsHGJH is prepare java";
 
 	public static void main(String[] args) {
 		
+		String s = "12736876";
+		
+		
 		System.out.println(txt.toUpperCase());
 		System.out.println(txt.toLowerCase());
-		System.out.println(txt.toLowerCase());
+		
+		
+		System.out.println(txt.indexOf(4));
+		System.out.println(txt.indexOf("is"));
+		
+		
+//		concatenation
+		
+		System.out.println(txt +" "+ s);
+		System.out.println(txt.concat(s));
 		
 		
 
