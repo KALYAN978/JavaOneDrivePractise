da2c84e9f81abde8246cf8e5b889822fce3c1dc0
Kalyan <pkalyanjs@gmail.com>
2024-04-18 12:29:38 +0530

    Methods practise code has been added

--- Java/W3/src/methods/Recursion2.java
+++ Java/W3/src/methods/Recursion2.java
@@ -3,8 +3,18 @@ package methods;
 public class Recursion2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int result = sum(5,10);
+		System.out.println(result);
+	}
+	
+	
+	public static int sum(int start, int end) {
+		if(end > start) {
+			return end + sum(start, end - 1);
+		} else {
+			return end;
+		}
 	}
 
 }
---
+++ Java/W3/src/oops/Main.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class Main {
+	
+	int x;
+
+	public static void main(String[] args) {
+		
+		Main m = new Main();
+		
+		m.x = 434;
+		System.out.println(m.x);
+
+	}
+
+}
