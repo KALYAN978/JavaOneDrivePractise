60c96ec6d26bb19fd02d5c693cc04e6afe273fcd
Kalyan <pkalyanjs@gmail.com>
2024-04-19 12:12:27 +0530

    ArrayList practise code has been added

---
+++ Java/W3/src/arraylist/ArrayList0.java
@@ -0,0 +1,29 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayList0 {
+	
+	
+	/*
+	 * Syntax: 
+	 * ArrayList<String> cars = new ArrayList<String>();
+	 * ArrayList<Integer> numbers = new ArrayList<Integer>();
+	 * ArrayList<Float> fNum = new ArrayList<Float>();
+	 * ArrayList<double> dnum = new ArrayList<Double<();
+	 */
+
+	public static void main(String[] args) {
+		
+		
+		ArrayList<String> cars = new ArrayList<String>();
+		
+		cars.add("ferrari");
+		cars.add("BMW");
+		cars.add("bolero");
+		
+		System.out.println(cars);
+
+	}
+
+}
