19b6cfcc37d9af9265c8d1bf4f08d8860381dd0d
Kalyan <pkalyanjs@gmail.com>
2024-04-19 11:22:00 +0530

    Methods practise code has been added

--- Java/W3/src/oops/AbstractClass.java
+++ Java/W3/src/oops/AbstractClass.java
@@ -1,9 +1,38 @@
 package oops;
 
+
+
+abstract class Mainn{
+	
+	public String fname = "John";
+	public int age = 24;
+	public abstract void study();    //Abstract Method 
+}
+
+
+
+class Primary extends Mainn{
+	
+	public int graduation = 20139;
+	
+	@Override
+	public void study() {    //the body of the abstract method is written here
+		System.out.println("Studying all day long");
+	}
+	
+}
+
 public class AbstractClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Primary p = new Primary();
+		
+		System.out.println(p.age);
+		System.out.println(p.fname);
+		System.out.println(p.graduation);
+		
+		p.study();
 
 	}
 
